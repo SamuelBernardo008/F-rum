223a6aff7f9f33df70a9288a91cc95be4b1b1dc7
Aluno Técnico ADS2 <samuelbernardorodrigues2008@gmail.com>
2026-03-10 16:52:15 -0300

    syle: página se login

--- static/script.js
+++ static/script.js
@@ -30,3 +30,17 @@ function toggleNotifs() {
             }
         }
 
+document.querySelectorAll('.link-ajax').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault(); // Impede o recarregamento da página
+        const url = this.getAttribute('href');
+
+        fetch(url)
+            .then(response => response.text())
+            .then(html => {
+                document.getElementById('conteudo-dinamico').innerHTML = html;
+                // Atualiza a URL no navegador sem recarregar
+                window.history.pushState({}, '', url); 
+            });
+    });
+});
